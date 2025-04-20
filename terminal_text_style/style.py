6a42398a99938c4fc3d7d96245555f652e9549ca
Victor Paiva <victor_eduardof@hotmail.com>
2025-04-19 23:09:28 -0300

    fix (style): fix error to select only one or less of total options

--- terminal_text_style/style.py
+++ terminal_text_style/style.py
@@ -8,14 +8,38 @@ nome = 'Victor'
 print(f'Hello, \033[4;33;44m{nome}\033[m!!!')
 '''
 
-def cor(type, text, back, message):
-    estilo = {'bold' : 1, 'italic' : 3, 'underline' : 4, 'negative': 7}
-    fonte = {'branco' : 30, 'vermelho' : 31, 'verde' : 32, 'amarelo' : 33, 'azul' : 34,
-    'roxo' : 35, 'ciano' : 36, 'cinza' : 37}
-    fundo = {'preto' : 40, 'vermelho' : 41, 'verde' : 42, 'amarelo' : 43, 'azul' : 44,
-    'roxo' : 45, 'ciano' : 46, 'cinza' : 47}
-    return f'\033[{estilo[type]};{fonte[text]};{fundo[back]}m{message}\033[m'
+def color_text(type=0, text=0, back=0, message=0):
+    styl = {0:0, 'bold' : 1, 'italic' : 3, 'underline' : 4, 'negative': 7, 'strike':9}
+    txt = {'black' : 30, 'red' : 31, 'green' : 32, 'yellow' : 33, 'blue' : 34,
+    'magenta' : 35, 'cyan' : 36, 'white' : 37}
+    background = {'black' : 40, 'red' : 41, 'green' : 42, 'yellow' : 43, 'blue' : 44,
+    'magenta' : 45, 'cyan' : 46, 'white' : 47}
+
+    if type== 0 and 0 not in [back, text, message]:
+        return f'\033[{txt[text]};{background[back]}m{message}\033[m'
+    elif text==0 and 0 not in [back, type, message]:
+        return f'\033[{styl[type]};{background[back]}m{message}\033[m'
+    elif back== 0 and 0 not in [type, text, message]:
+        return f'\033[{styl[type]};{txt[text]}m{message}\033[m'
+    elif type== 0 and text==0:
+        return f'\033[{background[back]}m{message}\033[m'
+    elif type== 0 and back== 0:
+        return f'\033[{txt[text]}m{message}\033[m'
+    elif text== 0 and back== 0:
+        return f'\033[{styl[type]}m{message}\033[m'
+    elif text== 0 and back== 0:
+        return f'\033[{styl[type]}m{message}\033[m'
+    else:
+        return f'\033[{styl[type]};{txt[text]};{background[back]}m{message}\033[m'
 
 if __name__ == '__main__':
-    print(cor('italic', 'ciano', 'roxo', 'Olá, mundo!'))
-    print(cor(text='verde', message='Abacaxi com goiaba')) #deu erro
+    print(color_text(type='bold', message='Abacaxi com goiaba'))
+    print(color_text(text='red', message='Abacaxi com goiaba'))
+    print(color_text(back='yellow', message='Abacaxi com goiaba'))
+    print(color_text(type='strike', back='green', message='Abacaxi com goiaba'))
+    print(color_text(type='bold', text='yellow', message='Abacaxi com goiaba'))
+    print(color_text(back='red', text='white', message='Abacaxi com goiaba'))
+    print(color_text(text='magenta', type='bold', back='black', message='Abacaxi com goiaba'))
+
+    print('------------------------------------')
+    print(f'\033[9;37;40mOlá mundo\033[m')
